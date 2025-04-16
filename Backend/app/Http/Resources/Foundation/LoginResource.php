80e50699600e581a0246146acee05a15f5a1e51c
ackyras <ackyrasibarani@gmail.com>
2025-04-16 17:10:11 +0700

    remove organization info on position

--- Backend/app/Http/Resources/Foundation/LoginResource.php
+++ Backend/app/Http/Resources/Foundation/LoginResource.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Resources\Foundation;
 
 use Illuminate\Http\Request;
@@ -10,21 +9,20 @@ class LoginResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'email' => $this->email,
-            'timezone' => $this->timezone,
-            'timezone_offset' => (float)$this->timezone_offset,
-            'organization' => [
-                'id' => $this->organization->id,
-                'name' => $this->organization->name,
+            'id'              => $this->id,
+            'first_name'      => $this->first_name,
+            'last_name'       => $this->last_name,
+            'email'           => $this->email,
+            'timezone'        => $this->timezone,
+            'timezone_offset' => (float) $this->timezone_offset,
+            'organization'    => [
+                'id'     => $this->organization->id,
+                'name'   => $this->organization->name,
                 'domain' => $this->organization->domain,
             ],
-            'position' => [
-                'id' => $this->position->id,
-                'name' => $this->position->name,
-                'organization_id' => $this->position->organization_id,
+            'position'        => [
+                'id'          => $this->position->id,
+                'name'        => $this->position->name,
                 'position_id' => $this->position->position_id,
             ],
         ];
