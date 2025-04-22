c83f3e9d92f6c9f7c90e258c7c42dc1d47da14df
ackyras <ackyrasibarani@gmail.com>
2025-04-22 19:14:40 +0700

    feat(clock in): match tiemstamp data

--- Backend/app/Http/Requests/Leave/StoreAttendanceRequest.php
+++ Backend/app/Http/Requests/Leave/StoreAttendanceRequest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Requests\Leave;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -15,19 +14,19 @@ class StoreAttendanceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            /** 
+            /**
              * @var int
              * @default 1
              * */
-            'type'          => ['required', Rule::in(1, 2), 'integer'],
-            'latitude'      => ['required', 'regex:/^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/'],
-            'longitude'     => ['required', 'regex:/^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$/'],
-            'image'         => ['required'],
+            'type'      => ['required', Rule::in(1, 2), 'integer'],
+            'latitude'  => ['required', 'regex:/^(\+|-)?(?:90(?:(?:\.0{1,20})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,20})?))$/'],
+            'longitude' => ['required', 'regex:/^(\+|-)?(?:180(?:(?:\.0{1,20})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,20})?))$/'],
+            'image'     => ['required'],
             /**
              * @var string|null
              * @default null
              */
-            'note'          => ['nullable'],
+            'note'      => ['nullable'],
         ];
     }
 }
--- Frontend/src/apis/v1/FileApi.js
+++ Frontend/src/apis/v1/FileApi.js
@@ -1,8 +1,8 @@
-import { GET, UPLOAD_S3 } from "@/configs/api";
+import { GET, POST, UPLOAD_S3 } from "@/configs/api";
 
 export default {
 	getPresignedUpload: (params) =>
-		GET({ path: `/foundation/file/upload`, params }),
+		POST({ path: `/foundation/file/upload`, params }),
 	getPresignedGet: (params) => GET({ path: `/foundation/file`, params }),
 	upload: ({ url, file }) => UPLOAD_S3({ url, file }),
 };
