7679f1a7889325e372fa5560e0cb7b045fce00c0
ikhbaaalll <mhmmadikhbal@gmail.com>
2025-05-09 02:17:44 +0700

    turing deus ex machina

--- Frontend/src/apis/v1/PayrollApi/PayrollPeriodApi.js
+++ Frontend/src/apis/v1/PayrollApi/PayrollPeriodApi.js
@@ -2,9 +2,9 @@ import { DELETE, GET, POST, PUT } from "@/configs/api";
 
 export default {
     getAll: (params) => GET({ path: `/payroll_period`, params }),
-    createPayrollPeriod: (params, ...payload) => POST({path: '/payroll_period/create'}),
+    createPayrollPeriod: (payload) => POST({ path: '/payroll_period/create', payload }),
     show: ({ id, ...payload }) =>
         PUT({ path: `/payroll_period/${id}`, payload }),
-    delete: ({ id, ...payload}) =>
-        DELETE({ path:`/payroll_period/${id}`, payload })
+    delete: ({ id, ...payload }) =>
+        DELETE({ path: `/payroll_period/${id}`, payload })
 };
--- Frontend/src/pages/Payroll/PayrollPeriod/partials/ModalAddPeriod.jsx
+++ Frontend/src/pages/Payroll/PayrollPeriod/partials/ModalAddPeriod.jsx
@@ -16,7 +16,7 @@ import { useCustomMutation } from "@/hooks/useCustomMutation";
 import HookFormProvider from "@/providers/HookFormProvider";
 import { TimesheetSchema } from "@/schema/request/Timesheet/FormTimesheetSchema";
 import {
-	calculateTimeDifferenceDays,
+    calculateTimeDifferenceDays,
 } from "@/services/helper";
 import PreviewTimesheet from "@/pages/Timesheet/MonthlyTimesheet/partials/PreviewTimesheet";
 import PayrollPeriodApi from "@/apis/v1/PayrollApi/PayrollPeriodApi";
@@ -25,105 +25,44 @@ import SelectControl from "@/components/moleculs/Control/SelectControl";
 import CheckboxControl from "@/components/moleculs/Control/CheckboxControl";
 
 const ModalAddPeriod = ({
-	handleFormOpen = () => {},
-	state: { state, type, event },
+    handleFormOpen = () => { },
+    state: { state, type, event },
 }) => {
-	const [detailState, setDetailState] = useState({
-		state,
-		type: [0, 1].includes(type) ? 0 : 1,
-		event,
-	});
+    const [detailState, setDetailState] = useState({
+        state,
+        type: [0, 1].includes(type) ? 0 : 1,
+        event,
+    });
 
-	const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-	const { onSubmit: onSubmitPayrollPeriod, isLoading: isLoadingSubmitPayrollPeriod } = 
-  useCustomMutation({
-    api: PayrollPeriodApi.createPayrollPeriod, // This should be your API call
-    onSuccess: (res) => {
-      queryClient.setQueryData(["payrollPeriod"], { data: res.data });
-      handleFormOpen({ isOpen: false }); // Close the form after success
-    },
-    onError: (err) => {
-      toast.error(err.message); // Show error message on failure
-    },
-    invalidateQueries: [["payrollPeriod", { id: state?.id }]],
-  });
+    const { onSubmit: onSubmitPayrollPeriod, isLoading: isLoadingSubmitPayrollPeriod } =
+        useCustomMutation({
+            api: PayrollPeriodApi.createPayrollPeriod, // This should be your API call
+            onSuccess: (res) => {
+                queryClient.setQueryData(["payrollPeriod"], { data: res.data });
+                handleFormOpen({ isOpen: false }); // Close the form after success
+            },
+            onError: (err) => {
+                toast.error(err.message); // Show error message on failure
+            },
+            invalidateQueries: [["payrollPeriod", { id: state?.id }], "payroll-period"],
+        });
 
-        const handleSubmit = (data, e) => {
-            const payload = {
-                month: data.month,
-                year: data.year,
-                start_date: data.start_date,
-                end_date: data.end_date,
-                is_generate_payrolls: data.is_generate_payrolls, // Include this field in the payload
-            };
-            
-            // Use the correct mutation handler here
-            onSubmitPayrollPeriod(payload, e);
+    const handleSubmit = (data) => {
+        const payload = {
+            month: data.month,
+            year: data.year,
+            startAt: format(data.start_date, "yyyy-MM-dd"),
+            endAt: format(data.end_date, "yyyy-MM-dd"),
+            payrollAt: format(data.payroll_at, "yyyy-MM-dd"),
+            is_generate_payrolls: data.is_generate_payrolls, // Include this field in the payload
         };
-        
-        
 
-    const PeriodDatePicker = () => {
-        const { watch, setValue } = useFormContext();
-        const [startDateAt, endDateAt] = watch([
-            "start_at",
-            "end_at",
-        ]);
-        return (
-            <div className="grid grid-cols-4 place-items-center gap-2 md:grid-cols-12">
-                <CalendarControl
-				name="start_at"
-				onChangeListen={(val) => {
-					if (!val) return;
-					if (!endDateAt || isAfter(val, endDateAt)) {
-						setValue("end_at", val);
-					}
-				}}
-				className="col-span-3"
-                />
-                <MoveRight className="hidden size-0 md:col-auto md:block md:size-5" />
-                <CalendarControl
-                    name="end_at"
-                    onChangeListen={(val) => {
-                        if (!val) return;
-                    }}
-                    calendarProps={{
-                        disabled: {
-                            before: startDateAt,
-                            after: add(startDateAt, { months: 1 }),
-                        },
-                    }}
-                className="col-span-3"
-                />  
-            </div>
-        );
+        console.log(payload);
+        onSubmitPayrollPeriod(payload);
     };
 
-    const PayrollDatePicker = () => {
-        const { watch } = useFormContext();
-        const [startDateAt, endDateAt] = watch(["start_at", "end_at"]);
-    
-        return (
-            <div className="my-2 flex flex-col gap-2">
-                <span className="font-normal">Payroll at</span>
-                <CalendarControl
-                    name="selected_date"
-                    onChangeListen={(val) => {
-                        if (!val) return;
-                    }}
-                    calendarProps={{
-                        disabled: {
-                            before: startDateAt,
-                            after: endDateAt,
-                        },
-                    }}
-                    className="col-span-4 md:col-span-3"
-                />
-            </div>
-        );
-    };
-    
     const months = [
         { id: 1, name: "January" },
         { id: 2, name: "February" },
@@ -138,82 +77,82 @@ const ModalAddPeriod = ({
         { id: 11, name: "November" },
         { id: 12, name: "December" },
     ];
-    
+
     const years = Array.from({ length: 10 }, (_, i) => {
         const year = new Date().getFullYear() - 5 + i;
         return { id: year, name: year.toString() };
     });
-    
+
     const [monthData] = useState(months);
     const [yearData] = useState(years);
-    
 
-	return (
-		<>
-        <HookFormProvider
-	onSubmit={handleSubmit}
-	defaultValues={{
-        month: null,
-        year: null,
-        start_date: null,
-        end_date: null,
-        is_generate_payrolls: false, // ✅ this is required
-      }}
-      
-	schema={AddPayrollPeriodSchema}
-	className="flex flex-col gap-3"
->
-	<SelectControl
-		label="Month"
-		name="month"
-		options={monthData.map((m) => ({
-			value: m.id,
-			label: m.name,
-		}))}
-	/>
 
-	<SelectControl
-		label="Year"
-		name="year"
-		options={yearData.map((y) => ({
-			value: y.id,
-			label: y.name,
-		}))}
-	/>
+    return (
+        <>
+            <HookFormProvider
+                onSubmit={handleSubmit}
+                defaultValues={{
+                    month: null,
+                    year: null,
+                    start_date: null,
+                    end_date: null,
+                    is_generate_payrolls: false, // ✅ this is required
+                }}
 
-	<CalendarControl
-		label="Start Date"
-		name="start_date"
-	/>
+                // schema={AddPayrollPeriodSchema}
+                className="flex flex-col gap-3"
+            >
+                <SelectControl
+                    label="Month"
+                    name="month"
+                    options={monthData.map((m) => ({
+                        value: m.id,
+                        label: m.name,
+                    }))}
+                />
 
-	<CalendarControl
-		label="End Date"
-		name="end_date"
-	/>
+                <SelectControl
+                    label="Year"
+                    name="year"
+                    options={yearData.map((y) => ({
+                        value: y.id,
+                        label: y.name,
+                    }))}
+                />
 
-	<CalendarControl
-		label="Payroll at"
-		name="payroll_at"
-	/>
+                <CalendarControl
+                    label="Start Date"
+                    name="start_date"
+                />
 
-    <CheckboxControl
-        name="is_generate_payrolls"
-        label="Generate employee payrolls?"
-        />
+                <CalendarControl
+                    label="End Date"
+                    name="end_date"
+                />
 
-	<div className="flex justify-end">
-		<Button disabled={isLoadingSubmitPayrollPeriod} className="w-40">
-			{isLoadingSubmitPayrollPeriod ? <Spinner /> : "Add Period"}
-		</Button>
-	</div>
-</HookFormProvider>
-		</>
-	);
+                <CalendarControl
+                    label="Payroll at"
+                    name="payroll_at"
+                />
+
+                <CheckboxControl
+                    name="is_generate_payrolls"
+                    label="Generate employee payrolls?"
+                />
+
+                <div className="flex justify-end">
+                    <Button disabled={isLoadingSubmitPayrollPeriod} className="w-40">
+                        {isLoadingSubmitPayrollPeriod ? <Spinner /> : "Add Period"}
+                    </Button>
+                </div>
+            </HookFormProvider>
+        </>
+    );
 };
 
 ModalAddPeriod.propTypes = {
-	handleFormOpen: PropTypes.func,
-	state: PropTypes.object,
+    handleFormOpen: PropTypes.func,
+    state: PropTypes.object,
 };
 
 export default ModalAddPeriod;
