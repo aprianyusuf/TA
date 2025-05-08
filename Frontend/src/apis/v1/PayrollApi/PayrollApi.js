d8f3041b315c575ac54407e0eed1f06789ce92be
ikhbaaalll <mhmmadikhbal@gmail.com>
2025-05-09 05:54:04 +0700

    awimbawe awimbawe

--- Frontend/src/apis/v1/PayrollApi/PayrollApi.js
+++ Frontend/src/apis/v1/PayrollApi/PayrollApi.js
@@ -2,9 +2,9 @@ import { DELETE, GET, POST, PUT } from "@/configs/api";
 
 export default {
     // getAll: (params) => GET({ path: `/payroll`, params }),
-    getPayrollsByPeriod: ({ id, ...payload }) =>
-        GET({ path: `/payroll_period/${id}/payrolls`, payload }),
-    show: (params) => GET({ path: `/payroll_period/${id}/payrolls`, params }),
-    showDetailPeriod: ({ id, periodId }) => GET({ path: `/payroll_period/${id}/payrolls/${periodId}` }),
+    getPayrollsByPeriod: ({ id, ...params }) =>
+        GET({ path: `/payroll_period/${id}/payrolls`, params }),
+    // show: (params) => GET({ path: `/payroll_period/${id}/payrolls`, params }),
+    showDetailPeriod: ({ id, periodId, ...params }) => GET({ path: `/payroll_period/${id}/payrolls/${periodId}`, params }),
     update: ({ id, ...payload }) => POST({ path: `/payroll/payrolls/update/${id}`, payload })
 };
