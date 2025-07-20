2129b36d3f6a9df07f2ad070b91ab3ecfc4426fc
aprianyusuf <aprian.118140144@student.itera.ac.id>
2025-07-20 23:47:11 +0700

    last

--- Frontend/src/pages/Payroll/PayrollPeriod/PayrollPreview.jsx
+++ Frontend/src/pages/Payroll/PayrollPeriod/PayrollPreview.jsx
@@ -12,6 +12,7 @@ import { useCustomQuery } from "@/hooks/useCustomQuery";
 import PayrollApi from "@/apis/v1/PayrollApi/PayrollApi";
 import { Spinner } from "@/components/atoms/Spinner";
 import NotFound from "@/templates/NotFound";
+import logo from '@/assets/kop_msp.png';
 import { Button } from "@/components/ui/Button";
 import { PrinterIcon } from "lucide-react";
 
@@ -58,9 +59,23 @@ const PayrollPreview = () => {
         return <NotFound message="Payroll not found" />;
     }
 
+    const toCurrency = (number) => new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+    }).format(number || 0);
+
+    const benefitNameMap = {
+        JHT: "Jaminan Hari Tua",
+        JKK: "Jaminan Keselamatan Kerja",
+        JKM: "Jaminan Kematian",
+        JP: "Jaminan Pensiun",
+        KS: "Jaminan Kesehatan",
+     };
+
+
     return (
         <>
-            <Card className="p-2">
+            {/* <Card className="p-2">
                 <CardHeader className="flex items-center justify-between p-6 text-center">
                     <CardTitle className="flex items-center justify-between w-full"><span>Payroll Detail</span>
                         <Button onClick={handlePrint} size="sm">
@@ -162,6 +177,125 @@ const PayrollPreview = () => {
                         </div>
                     </div>
                 </CardContent>
+            </Card> */}
+            <Card className="p-6">
+                {/* <CardHeader>
+                    <div className="flex items-center gap-60">
+                        <img src={logo} alt="Logo" className="h-20" />
+                        <div className="flex flex-col items-start">
+                            <h2 className="text-xl font-bold">Slip Gaji</h2>
+                            <div className="w-full border-b border-black my-1" />
+                            <div className="text-sm font-bold">
+                                {format(new Date(), 'dd - MMM').toUpperCase()}
+                            </div>
+                        </div>
+                        <Button onClick={handlePrint} size="sm">
+                            <PrinterIcon size={15} />
+                        </Button>
+                    </div>
+                </CardHeader> */}
+
+                <CardContent ref={pdfRef} className="space-y-4">
+                    <div className="flex items-center gap-60">
+                        <img src={logo} alt="Logo" className="h-20" />
+                        <div className="flex flex-col items-start">
+                            <h2 className="text-xl font-bold">Slip Gaji</h2>
+                            <div className="w-full border-b border-black my-1" />
+                            <div className="text-sm font-bold">
+                                {format(new Date(), 'dd - MMM').toUpperCase()}
+                            </div>
+                        </div>
+                        <Button onClick={handlePrint} size="sm">
+                            <PrinterIcon size={15} />
+                        </Button>
+                    </div>
+                    <div className="text-xl">
+                        <div><strong>Nama :</strong> {`${detailPayroll.data[0].firstName} ${detailPayroll.data[0].lastName}`}</div>
+                    </div>
+
+                    <div className="grid grid-cols-2 gap-6 text-sm">
+                        <div>
+                            <h3 className="text-center font-bold border-b pb-1">Penerimaan</h3>
+                            <table className="w-full">
+                                <tbody>
+                                    <tr>
+                                        <td className="text-left py-1">Gaji Pokok</td>
+                                        <td className="text-right py-1">{toCurrency(detailPayroll.data[0].salary)}</td>
+                                    </tr>
+                                    {detailPayroll.data[0].bonuses.map(({ name, value }, idx) => (
+                                        <tr key={idx}>
+                                            <td className="py-1 text-left">{benefitNameMap[name] || name}</td>
+                                            {/* <td>{value}</td> */}
+                                            <td className="py-1 text-right">{new Intl.NumberFormat('id-ID', {
+                                                style: 'currency',
+                                                currency: 'IDR',
+                                            }).format(value / 100 * detailPayroll.data[0].salary)}</td>
+                                        </tr>
+                                    ))}
+                                    <tr><td className="text-left py-1">Intensif/Target</td><td className="py-1 text-right">-</td></tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr className="font-bold border-t">
+                                        <td className="py-1 text-left">Total</td>
+                                        <td className="font-bold  py-1 text-right">{new Intl.NumberFormat('id-ID', {
+                                            style: 'currency',
+                                            currency: 'IDR',
+                                        }).format(detailPayroll.data[0].bonuses.reduce((prev, next) => prev + (next.value / 100 * detailPayroll.data[0].salary), 0))}</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+
+                        <div>
+                            <h3 className="text-center font-bold border-b pb-1">Potongan</h3>
+                            <table className="w-full">
+                                <tbody>
+                                    {detailPayroll.data[0].deductions.map(({ name, value }, idx) => (
+                                        <tr key={idx}>
+                                            <td className="py-1 text-left">{benefitNameMap[name] || name}</td>
+                                            {/* <td>{value}</td> */}
+                                            <td className="py-1 text-right">{new Intl.NumberFormat('id-ID', {
+                                                style: 'currency',
+                                                currency: 'IDR',
+                                            }).format(value / 100 * detailPayroll.data[0].salary)}</td>
+                                        </tr>
+                                    ))}
+                                    <tr><td className="py-1 text-left;">Kas Bon</td><td className="py-1 text-right">-</td></tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr className="font-bold border-t">
+                                        <td className="py-1 text-left">Total</td>
+                                        <td className="font-bold py-1 text-right">{new Intl.NumberFormat('id-ID', {
+                                            style: 'currency',
+                                            currency: 'IDR',
+                                        }).format(detailPayroll.data[0].deductions.reduce((prev, next) => prev + (next.value / 100 * detailPayroll.data[0].salary), 0))}</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div className="text-xl font-bold text-right border-t pt-4">
+                        Total Terima: {toCurrency(detailPayroll.data[0].netPay)}
+                    </div>
+                <div className="mt-12 grid grid-cols-3 text-sm text-center">
+                    <div>
+                        <div className="mb-24">Mengetahui,</div>
+                        <div className="underline font-bold">
+                            Noni
+                        </div>
+                        <div>Acc</div>
+                    </div>
+                    <div></div>
+                    <div>
+                        <div className="mb-24">Bandar Lampung, {format(new Date(), 'dd MMMM yyyy')}</div>
+                        <div className="underline font-bold">
+                            {`${detailPayroll.data[0].firstName} ${detailPayroll.data[0].lastName}`}
+                        </div>
+                    </div>
+                </div>
+                </CardContent>
+
             </Card>
 
         </>
